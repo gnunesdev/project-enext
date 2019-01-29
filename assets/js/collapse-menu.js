556e8373c5204a62c99ceaef340e00a9f0ea26c3
gnunes <guilherme.teixeira@isolutionscom.br>
2019-01-29 17:47:22 -0200

    collapse menu changes

--- assets/js/collapse-menu.js
+++ assets/js/collapse-menu.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
   $(".menu-icon").on("click", function() {
     $(".nav-menu").toggleClass("displayBlock");
-    $(".bag").toggleClass("displayNone");
-    $(".logo").toggleClass("displayNone");
-    $(".free-shipping").toggleClass("displayNone");
   });
 });
